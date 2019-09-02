af137d465ccd7a6d4c039a6e1a0e03319b1b25f5
Rana Saeed <ranas.2122@gmail.com>
2019-09-02 15:45:42 +0200

    feat: add shift encrypt

--- task.py
+++ task.py
@@ -1,3 +1,20 @@
 import sys
+from string import ascii_lowercase as ALPHABETLOWER
+from string import ascii_uppercase as ALPHABETUPPER
 
-sys.stdout.write("hello from Python %s\n" % (sys.version,))
+def shiftEncrypt(s):
+	offset = 3
+	output = ""
+
+	for c in s:
+		if c in ALPHABETLOWER:
+			output = ''.join((output, chr(ord(c) + offset)))
+		elif c in ALPHABETUPPER:
+			output = ''.join((output, chr(ord(c) + offset)))
+		else:
+			output = ''.join((output, c))
+	
+	print(output)
+	
+
+shiftEncrypt("Hello World")
